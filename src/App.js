3673cb918adc1f01c98bd073df7c73fe7fc4e152
marckimbrey <marckimbrey@gmail.com>
2016-08-14 06:50:53 +0100

    add fetch requests

--- src/App.js
+++ src/App.js
@@ -2,28 +2,27 @@ import React, { Component } from 'react';
 
 import LeaderBoard from "./leaderboard";
 class App extends Component {
-  constructor(props) {
-    super(props)
+  constructor() {
+    super();
     this.state = {users: []};
     this.updateTable("https://fcctop100.herokuapp.com/api/fccusers/top/recent");
+    this.updateTable = this.updateTable.bind(this);
   }
 
   updateTable(url) {
     fetch(url, {method: "GET"}).then((response) => {
-        return response.json();
-      }).then((val) => {
-        this.setState({users: val});
-    })
-    // let req = new XMLHttpRequest();
-    // req.open("GET", url, true);
-    // req.send(null);
-    // this.setState(req);
-
+      return response.json();
+    }).then((val) => {
+      return this.setState({users: val}, () => {});
+    });
   }
   render() {
     return (
       <div className="App">
-        <LeaderBoard test={this.state.users}/>
+        <LeaderBoard
+          users={this.state.users}
+          fetchNewData={this.updateTable}
+        />
       </div>
     );
   }
--- src/leaderboard.js
+++ src/leaderboard.js
@@ -2,19 +2,21 @@ import React from "react";
 import LeaderBoardRow from "./leaderboardRow";
 
 export default (props) => {
+  const recent = "https://fcctop100.herokuapp.com/api/fccusers/top/recent";
+  const allTime = "https://fcctop100.herokuapp.com/api/fccusers/top/alltime";
   let rows = [];
-   props.test.forEach((camper) => {
+   props.users.forEach((camper) => {
      rows.push(<LeaderBoardRow camper={camper} key={camper.username} />);
    });
 
   return (
     <table>
       <thead>
-      <tr>
-        <th>Camper</th>
-        <th>Last Month</th>
-        <th>All Time</th>
-      </tr>
+        <tr>
+          <th>Camper</th>
+          <th><a href="#" onClick={() => {props.fetchNewData(recent)}} >Recent</a></th>
+          <th><a href="#" onClick={() => {props.fetchNewData(allTime)}} >All Time</a></th>
+        </tr>
       </thead>
       <tbody>
         {rows}
